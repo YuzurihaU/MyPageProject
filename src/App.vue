699894dbe30197c514c8bfbc402a385acd9bbccb
EC2 Default User <ec2-user@ip-172-31-20-102.ap-northeast-1.compute.internal>
2021-08-31 03:07:18 +0000

    app verの編集他

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,58 @@
 <template>
   <v-app>
+    <!--　header　-->
     <v-app-bar
       app
       color="primary"
       dark
     >
       <div class="d-flex align-center">
+        <v-app-bar-nav-icon @click="drawer = true">
+        </v-app-bar-nav-icon>
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="../src/assets/logo.png"
           transition="scale-transition"
-          width="40"
+          width="50"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+        </div>
+        <div class="text-h4 -weight-medium">
+          Portfolio
+        </div>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-
+    <!--　nav展開部　-->
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <!--<v-list-item>-->
+          <!--  <v-list-item-icon>-->
+          <!--    <v-icon>mdi-account</v-icon>-->
+          <!--  </v-list-item-icon>-->
+          <!--  <v-list-item-title>Account</v-list-item-title>-->
+          <!--</v-list-item>-->
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!--　メインコンテンツ描画 -->
     <v-main>
       <router-view/>
     </v-main>
@@ -49,7 +65,8 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
